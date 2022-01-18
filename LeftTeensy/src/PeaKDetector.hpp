f1a2e37e17b3a872223b660d3e345c31fae6c82b
“Jupiter <bautista@gmx.de>
2022-01-18 09:49:26 +0100

    Reciver Peakdetector Added

---
+++ LeftTeensy/src/PeaKDetector.hpp
@@ -0,0 +1,73 @@
+#ifndef PEAKDETECTOR_H
+#define PEAKDETECTOR_H
+#pragma once
+#include "Arduino.h"
+
+class PeakDetector
+{
+private:
+	int m_thresholdMin     {60} ; // minimum reading, avoid noise and false starts
+    int m_peakTrackMillis  {12} ;
+	int m_aftershockMillis {25} ; // aftershocks & vibration reject
+	int m_inputSensor      { 0} ;
+
+    int           m_state;// 0=idle, 1=looking for peak, 2=ignore aftershocks
+    int           m_peak; // remember the highest reading
+    elapsedMillis m_msec; // timer to end states 1 and 2
+
+public:
+	PeakDetector(int inputSensor):inputSensor(m_inputSensor)
+	{}
+
+
+	void loop()
+	{
+		switch (m_state)
+		{
+
+		// IDLE state: wait for any reading is above threshold.  Do not set
+		// the threshold too low.  You don't want to be too sensitive to slight
+		// vibration.
+		case 0:
+			if (m_inputSensor > m_thresholdMin)
+			{
+				//Serial.print("begin peak track ");
+				//Serial.println(voltage);
+				m_peak = m_inputSensor;
+				m_msec = 0;
+				m_state = 1;
+			}
+			return;
+
+
+		// Peak Tracking state: capture largest reading
+		case 1:
+			if (m_inputSensor > m_peak)
+			{
+				m_peak = m_inputSensor;
+			}
+			if (m_msec >= m_peakTrackMillis)
+			{
+				//Serial.print("peak = ");
+				//Serial.println(peak);
+				m_msec = 0;
+				m_state = 2;
+			}
+			return;
+
+
+		// Ignore Aftershock state: wait for things to be quiet again.
+		default:
+			if (m_inputSensor > m_thresholdMin)
+			{
+				m_msec = 0; // keep resetting timer if above threshold
+			}
+			else if (m_msec > m_aftershockMillis)
+			{
+				//usbMIDI.sendNoteOff(note, 0, channel);
+				m_state = 0; // go back to idle when
+			}
+		}
+	}
+};
+#endif
--- LeftTeensy/src/main.cpp
+++ LeftTeensy/src/main.cpp
@@ -1,9 +1,47 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include "printf.h"
+#include "RF24.h"
+#include "Reciver.hpp"
+#include "ReciverData.hpp"
+#include "PeaKDetector.hpp"
 
-void setup() {
-  // put your setup code here, to run once:
+
+/*
+██████╗ ███████╗██████╗ ██╗  ██╗
+██╔══██╗██╔════╝╚════██╗██║  ██║
+██████╔╝█████╗   █████╔╝███████║
+██╔══██╗██╔══╝  ██╔═══╝ ╚════██║
+██║  ██║██║     ███████╗     ██║
+╚═╝  ╚═╝╚═╝     ╚══════╝     ╚═╝
+*/
+
+RF24                   radio(21, 20); //CE pin, CSN pin
+const uint64_t ADRESS  {0xF0F0F0F0E1LL};
+const byte     CHANNEl {125};               
+ReciverData racketData;
+Reciver reciver(radio, ADRESS, CHANNEL, racketData);
+
+PeakDetector racketPiezo(racketData.pz);
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial)
+  {
+  }
+
+  reciver.setup();
+ 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  reciver.loop();
+  racketPiezo.loop();
+  //racketPiezo.getPeak();
+  //racketPiezo
+
 }
